05a3fecc170e1c69e62f3060ddaacadd331a4353
Rodin Grajo <scaredmexicanlaborer@protonmail.com>
2023-04-07 00:22:01 -0400

    testing again

--- src/App.tsx
+++ src/App.tsx
@@ -8,7 +8,7 @@ function App() {
   const [count, setCount] = useState(0)
 
   useEffect(() => {
-    console.log("pls work", supabaseClient);
+    console.log("pls work crap", supabaseClient);
   }, []);
 
   return (
