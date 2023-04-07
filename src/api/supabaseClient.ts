be52ee167ceb6a1bf007417992042620fea088b9
Rodin Grajo <scaredmexicanlaborer@protonmail.com>
2023-04-07 00:25:11 -0400

    pls work

--- src/api/supabaseClient.ts
+++ src/api/supabaseClient.ts
@@ -3,4 +3,6 @@ import { createClient } from '@supabase/supabase-js'
 const supabaseUrl = import.meta.env.SUPABASE_URl;
 const anonKey = import.meta.env.SUPABASE_ANON_KEY;
 
+console.log("supabaseUrl:", supabaseUrl);
+
 export const supabaseClient = createClient(supabaseUrl, anonKey);
