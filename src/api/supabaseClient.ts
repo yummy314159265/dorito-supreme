438d7544eaef3d3d4c23a799a46041c21de5c871
Rodin Grajo <scaredmexicanlaborer@protonmail.com>
2023-04-07 00:29:17 -0400

    fixed typo ugh

--- src/api/supabaseClient.ts
+++ src/api/supabaseClient.ts
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js'
 
-const supabaseUrl = process.env.SUPABASE_URl ?? "";
+const supabaseUrl = process.env.SUPABASE_URL ?? "";
 const anonKey = process.env.SUPABASE_ANON_KEY ?? "";
 
 console.log("supabaseUrl:", supabaseUrl);
